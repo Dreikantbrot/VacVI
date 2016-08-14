f0573aeca820c1ae492492d4c3f28ca5c149334f
Scavenger4711 <haberland.kevin@vodafone.de>
2016-08-15 00:19:59 +0200

    Improved UI
    - deleted labels
    - implemented custom draw routine (prototype)

--- Overlay.cs
+++ Overlay.cs
@@ -2,25 +2,36 @@
 using System;
 using System.Threading;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Evo_VI
 {
     public partial class Overlay : Form
     {
-        public static Label lbl_dbg;
-        public static string lbl_dbg_oriTxt;
-
+        string _text = "";
         public Overlay()
         {
             InitializeComponent();
 
-            lbl_dbg = lbl_OverlayTitle;
-            lbl_dbg_oriTxt = lbl_OverlayTitle.Text;
+            /* Initialize UI */
+            _text = this.Text;
 
             /* Initialize all components */
             SpeechEngine.Initialize();
             Interactor.Initialize();
             VI.Initialize();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            string info = "\n\n" + DateTime.UtcNow.ToLongDateString() + "\n" + DateTime.Now.ToLongTimeString();
+            TextRenderer.DrawText(e.Graphics, _text + info, this.Font, new Point(10, 10), SystemColors.ControlText);
+
+            Font debugFont = new System.Drawing.Font(this.Font.FontFamily, 8, this.Font.Style);
+            e.Graphics.DrawLine(new Pen(SystemColors.ControlText), 5, this.Height - 40 - 5, this.Width - 5, this.Height - 40 - 5);
+            TextRenderer.DrawText(e.Graphics, "Debug", debugFont, new Point(10, this.Height - 40), SystemColors.ControlText);
+
+            this.Invalidate();
         }
     }
 }
